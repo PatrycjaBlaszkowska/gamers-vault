687806c2fd6891a46f86d222e0b7bec9580e12dd
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-09-01 16:13:17 +0000

    Added category filtering.

--- products/views.py
+++ products/views.py
@@ -1,31 +1,41 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Subcategory
 
 def all_products(request):
     """ A view to show all products, including sorting and searching queries. """
-
+    
     products = Product.objects.all()
     query = None
+    categories = Category.objects.all()  
+    subcategories = Subcategory.objects.all()
 
     if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-        if not query:
-            messages.error(request, "You didn't enter any search criteria!")
-            return redirect(reverse('products'))
+        category_filter = request.GET.get('category', None)
+        subcategory_filter = request.GET.get('subcategory', None)
 
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
+        if category_filter:
+            products = products.filter(category__name=category_filter)
+
+        if subcategory_filter:
+            products = products.filter(subcategory__name=subcategory_filter)
+
+        if query := request.GET.get('q', None):
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        'products' : products,
-        'search_term' : query,
+        'products': products,
+        'search_term': query,
+        'categories': categories,  
+        'subcategories': subcategories, 
     }
 
     return render(request, 'products/products.html', context)
-
 
 def product_details(request, product_id):
     """ A view to show individual product details. """
