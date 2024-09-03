8558d04a2f907f00b8a2ba2a9a3c02e762913dc5
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-09-03 12:14:42 +0000

    Amended views for home page in order to fix subcategories which weren't passed into the template.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from products.models import Category, Subcategory
 
 def index(request):
     """ A view to return home page. """
 
-    return render(request, 'home/index.html')
+    categories = Category.objects.all()  
+    subcategories = Subcategory.objects.all()
+    
+    context = {
+        'categories': categories,
+        'subcategories': subcategories,
+    }
+
+    return render(request, 'home/index.html',context)
