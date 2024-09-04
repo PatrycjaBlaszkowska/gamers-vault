b1a1c12b5c9a85ac2506c67fdd7867c5f21ae4c4
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-09-04 20:21:37 +0000

    Added toasts and changed navbar from fixed to sticky as it was overlapping toasts.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect,reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 def view_bag(request):
     """ A view that renders the bag page. """
@@ -9,6 +12,8 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag. """
 
+    product = Product.objects.get(pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -17,6 +22,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to the bag.')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -43,7 +49,7 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
         if item_id in bag:
             bag.pop(item_id)  
-            
+
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
