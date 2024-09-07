196d3426f867ffec1d6b643a0a785facb72ad9e1
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-09-07 01:35:50 +0000

    Added gunicorn amd whitenoise.

--- gamers_vault/settings.py
+++ gamers_vault/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os 
 import dj_database_url
+import django_heroku
 
 if os.path.isfile('env.py'):
     import env
@@ -157,6 +158,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
 
 # Stripe
 
