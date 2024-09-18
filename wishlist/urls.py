334f379aeedd6105002e4ce0e522043c490ffdd3
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-09-18 18:34:03 +0000

    Added views and urls for user to add product to their wishlist as well as view them.

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('wishlist/', views.wishlist_view, name='wishlist'),
+    path('', views.wishlist_view, name='wishlist'),
+    path('add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Wishlist
+from products.models import Product
 
-@login_required 
+
+@login_required
 def wishlist_view(request):
     # Get all the wishlist items for the current logged-in user
     wishlist_items = Wishlist.objects.filter(user=request.user).select_related('product')
     
-    template = 'wishlist/wishlist.html'
     context = {
         'wishlist_items': wishlist_items,
     }
 
-    return render(request, template, context)
+    return render(request, 'wishlist/wishlist.html', context)
+
+@login_required
+def add_to_wishlist(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    Wishlist.objects.get_or_create(user=request.user, product=product)
+    
+    return redirect('products')
