3cbbbd56cecbddbad1f56bf3385a3c75bfc5791d
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-09-23 12:36:46 +0000

    Added some padding and margins to make it responsive on mobile devices.

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = 'static'
+    location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
+    location = settings.MEDIAFILES_LOCATION
