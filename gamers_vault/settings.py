e6d4ccdbe4e5a7af182d776df1461c320d461448
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-10-03 18:51:43 +0000

    Added shop's name to the default email address for better user experiance.

--- gamers_vault/settings.py
+++ gamers_vault/settings.py
@@ -218,4 +218,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = f"Gamers Vault <{os.environ.get('EMAIL_HOST_USER')}>"
