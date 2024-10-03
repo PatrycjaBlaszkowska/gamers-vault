fe35e9382bed33e3f05943ee97e4d6c1d63e1336
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-10-03 18:34:31 +0000

    Fixed a bug on the wishlist causing ratings not to show.

--- gamers_vault/settings.py
+++ gamers_vault/settings.py
@@ -210,7 +210,7 @@ STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'gamers_vault@gmail.com'
+    DEFAULT_FROM_EMAIL = 'gamers.vault@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
